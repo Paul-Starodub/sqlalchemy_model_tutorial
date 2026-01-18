d2b3691db124ff99665897d4dcc2578417e805bd
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-18 18:25:24 +0200

    Add async lifespan handler to initialize database tables

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
+from core.models import db_helper, Base
 from users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(users_router)
 
 
