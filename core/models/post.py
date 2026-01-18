ff43d6a7283f7a7e4bebecef67d9c73c510c307a
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-18 11:56:25 +0200

    Add user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,17 +1,15 @@
-from typing import TYPE_CHECKING
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 from core.models.base import Base
-
-if TYPE_CHECKING:
-    from core.models.user import User
+from core.models.mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    __tablename__ = "posts"
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text(200), default="", server_default="")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))  # Foreign key to User
-    user: Mapped["User"] = relationship(back_populates="posts")
 
     def __repr__(self) -> str:
         return f"Post {self.title}"
--- core/models/profile.py
+++ core/models/profile.py
@@ -1,20 +1,18 @@
-from typing import TYPE_CHECKING
-from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 from core.models.base import Base
-
-if TYPE_CHECKING:
-    from core.models.user import User
+from core.models.mixins import UserRelationMixin
 
 
-class Profile(Base):
+class Profile(UserRelationMixin, Base):
     __tablename__ = "profiles"
+
+    _user_id_unique = True
+    _user_back_populates = "profile"
 
     first_name: Mapped[str | None] = mapped_column(String(32))
     last_name: Mapped[str | None] = mapped_column(String(32))
     bio: Mapped[str | None]
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)  # Foreign key to User (One-to-One)
-    user: Mapped["User"] = relationship(back_populates="profile")
 
     def __repr__(self) -> str:
         return f"Profile {self.first_name} {self.last_name}"
